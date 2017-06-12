c3043747ff28614afdbecbaa0250478d1b9ac7ac
ravichandran1383 <contact@bigspire.com>
2017-06-12 15:06:28 +0530

    12 june change

--- hiring/configs/dbconfig.php
+++ hiring/configs/dbconfig.php
@@ -5,7 +5,7 @@ Created : Nikitasa
 Date : 21-01-2017
 */
 // db config
-define('Host', 'localhost');
+define('Host', '172.20.10.13');
 define('Username', 'cthire');
 define('Password', 'hire938');
 define('Database', 'cthiring_dbase');
