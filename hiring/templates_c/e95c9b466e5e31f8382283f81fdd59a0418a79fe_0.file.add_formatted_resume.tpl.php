cf3948bb4164b96d51ec1c2cce7a43bce0c59725
ravichandran1383 <contact@bigspire.com>
2017-07-27 11:26:03 +0530

    changes

--- hiring/templates_c/e95c9b466e5e31f8382283f81fdd59a0418a79fe_0.file.add_formatted_resume.tpl.php
+++ hiring/templates_c/e95c9b466e5e31f8382283f81fdd59a0418a79fe_0.file.add_formatted_resume.tpl.php
@@ -1,17 +1,17 @@
 <?php
-/* Smarty version 3.1.29, created on 2017-07-26 12:41:53
+/* Smarty version 3.1.29, created on 2017-07-26 16:29:39
   from "F:\xampp\htdocs\ctsvn\cthiring\hiring\templates\add_formatted_resume.tpl" */
 
 if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
   'has_nocache_code' => false,
   'version' => '3.1.29',
-  'unifunc' => 'content_597840b93ffd31_02092388',
+  'unifunc' => 'content_5978761b362845_61339765',
   'file_dependency' => 
   array (
     'e95c9b466e5e31f8382283f81fdd59a0418a79fe' => 
     array (
       0 => 'F:\\xampp\\htdocs\\ctsvn\\cthiring\\hiring\\templates\\add_formatted_resume.tpl',
-      1 => 1500044213,
+      1 => 1501066718,
       2 => 'file',
     ),
   ),
@@ -21,7 +21,7 @@ if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl,
     'file:include/footer.tpl' => 1,
   ),
 ),false)) {
-function content_597840b93ffd31_02092388 ($_smarty_tpl) {
+function content_5978761b362845_61339765 ($_smarty_tpl) {
 if (!is_callable('smarty_function_html_options')) require_once 'F:\\xampp\\htdocs\\ctsvn\\cthiring\\hiring\\vendor\\smarty-3.1.29\\libs\\plugins\\function.html_options.php';
 ?>
 
@@ -162,12 +162,12 @@ echo $_smarty_tpl->tpl_vars['last_nameErr']->value;?>
 										<td>
 										<select name="year_of_exp" tabindex="9" class="span4">
 										<option value="">Year</option>
-										<?php echo smarty_function_html_options(array('options'=>$_smarty_tpl->tpl_vars['exp_yr']->value,'selected'=>$_smarty_tpl->tpl_vars['year_of_exp']->value),$_smarty_tpl);?>
+										<?php echo smarty_function_html_options(array('options'=>$_smarty_tpl->tpl_vars['total_exp_yr']->value,'selected'=>$_smarty_tpl->tpl_vars['year_of_exp']->value),$_smarty_tpl);?>
 	
 										</select>
 										<select name="month_of_exp" tabindex="10" class="inline_text span4">
 										<option value="">Month</option>
-										<?php echo smarty_function_html_options(array('options'=>$_smarty_tpl->tpl_vars['exp_month']->value,'selected'=>$_smarty_tpl->tpl_vars['month_of_exp']->value),$_smarty_tpl);?>
+										<?php echo smarty_function_html_options(array('options'=>$_smarty_tpl->tpl_vars['total_exp_month']->value,'selected'=>$_smarty_tpl->tpl_vars['month_of_exp']->value),$_smarty_tpl);?>
 	
 										</select>
 										<label for="reg_city" generated="true" class="error"><?php echo $_smarty_tpl->tpl_vars['year_of_expErr']->value;
@@ -509,22 +509,35 @@ echo $_POST['hobby'];
 										</td>
 							</tr>
 								<tr>
-										<td width="120" class="tbl_column">Employment Period<span class="f_req">*</span></td>
+										<td width="120" class="tbl_column">Employment Period<span class="f_req"> *</span></td>
 										<td>
-										<select name="year_of_exp_#index#" id = "year_of_exp_#index#" tabindex="2" class="span4">
-										<option value="">Year</option>
+										<select name="from_month_of_exp_#index#" id = "from_month_of_exp_#index#" tabindex="3" class="span3">
+										<option value="">From Month</option>
+										<?php echo smarty_function_html_options(array('options'=>$_smarty_tpl->tpl_vars['exp_month']->value),$_smarty_tpl);?>
+ 
+										</select>
+										<select name="from_year_of_exp_#index#" id = "from_year_of_exp_#index#" tabindex="2" class="inline_text span3">
+										<option value="">From Year</option>
 										<?php echo smarty_function_html_options(array('options'=>$_smarty_tpl->tpl_vars['exp_yr']->value),$_smarty_tpl);?>
  
 										</select>
 										
-										<select name="month_of_exp_#index#" id = "month_of_exp_#index#" tabindex="3" class="inline_text span4">
-										<option value="">Month</option>
+										<select name="to_month_of_exp_#index#" id = "to_month_of_exp_#index#" tabindex="3" class="inline_text span3">
+										<option value="">To Month</option>
 										<?php echo smarty_function_html_options(array('options'=>$_smarty_tpl->tpl_vars['exp_month']->value),$_smarty_tpl);?>
  
 										</select>
 										
-										<label for="reg_city" generated="true" class="error" id="year_of_expErr_#index#"></label>																						
-										<label for="reg_city" generated="true" class="error" id="month_of_expErr_#index#"></label>																						
+										<select name="to_year_of_exp_#index#" id = "to_year_of_exp_#index#" tabindex="2" class="inline_text span3">
+										<option value="">To Year</option>
+										<?php echo smarty_function_html_options(array('options'=>$_smarty_tpl->tpl_vars['exp_yr']->value),$_smarty_tpl);?>
+ 
+										</select>
+										<label for="reg_city" generated="true" class="error" id="from_month_of_expErr_#index#"></label>																																
+										<label for="reg_city" generated="true" class="error" id="from_year_of_expErr_#index#"></label>																						
+										<label for="reg_city" generated="true" class="error" id="to_month_of_expErr_#index#"></label>																						
+										<label for="reg_city" generated="true" class="error" id="to_year_of_expErr_#index#"></label>																						
+										
 										</td>
 							</tr>
 							<tr class="tbl_row">
@@ -939,13 +952,21 @@ for ($_foo=true;$_smarty_tpl->tpl_vars['i']->value < $_smarty_tpl->tpl_vars['exp
 " name="areaData_<?php echo $_smarty_tpl->tpl_vars['i']->value;?>
 " value="<?php echo $_smarty_tpl->tpl_vars['areaData']->value[$_smarty_tpl->tpl_vars['i']->value];?>
 ">
-		<input type="hidden" id="year_of_expData_<?php echo $_smarty_tpl->tpl_vars['i']->value;?>
-" name="year_of_expData_<?php echo $_smarty_tpl->tpl_vars['i']->value;?>
-" value="<?php echo $_smarty_tpl->tpl_vars['year_of_expData']->value[$_smarty_tpl->tpl_vars['i']->value];?>
+		<input type="hidden" id="from_year_of_expData_<?php echo $_smarty_tpl->tpl_vars['i']->value;?>
+" name="from_year_of_expData_<?php echo $_smarty_tpl->tpl_vars['i']->value;?>
+" value="<?php echo $_smarty_tpl->tpl_vars['from_year_of_expData']->value[$_smarty_tpl->tpl_vars['i']->value];?>
 ">
-		<input type="hidden" id="month_of_expData_<?php echo $_smarty_tpl->tpl_vars['i']->value;?>
-" name="month_of_expData_<?php echo $_smarty_tpl->tpl_vars['i']->value;?>
-" value="<?php echo $_smarty_tpl->tpl_vars['month_of_expData']->value[$_smarty_tpl->tpl_vars['i']->value];?>
+		<input type="hidden" id="from_month_of_expData_<?php echo $_smarty_tpl->tpl_vars['i']->value;?>
+" name="from_month_of_expData_<?php echo $_smarty_tpl->tpl_vars['i']->value;?>
+" value="<?php echo $_smarty_tpl->tpl_vars['from_month_of_expData']->value[$_smarty_tpl->tpl_vars['i']->value];?>
+">
+		<input type="hidden" id="to_year_of_expData_<?php echo $_smarty_tpl->tpl_vars['i']->value;?>
+" name="to_year_of_expData_<?php echo $_smarty_tpl->tpl_vars['i']->value;?>
+" value="<?php echo $_smarty_tpl->tpl_vars['to_year_of_expData']->value[$_smarty_tpl->tpl_vars['i']->value];?>
+">
+		<input type="hidden" id="to_month_of_expData_<?php echo $_smarty_tpl->tpl_vars['i']->value;?>
+" name="to_month_of_expData_<?php echo $_smarty_tpl->tpl_vars['i']->value;?>
+" value="<?php echo $_smarty_tpl->tpl_vars['to_month_of_expData']->value[$_smarty_tpl->tpl_vars['i']->value];?>
 ">
 		<input type="hidden" id="companyData_<?php echo $_smarty_tpl->tpl_vars['i']->value;?>
 " name="companyData_<?php echo $_smarty_tpl->tpl_vars['i']->value;?>
@@ -979,11 +1000,17 @@ for ($_foo=true;$_smarty_tpl->tpl_vars['i']->value < $_smarty_tpl->tpl_vars['exp
 		<input type="hidden" id="desig_Err_Data_<?php echo $_smarty_tpl->tpl_vars['i']->value;?>
 "  value="<?php echo $_smarty_tpl->tpl_vars['expErr']->value[$_smarty_tpl->tpl_vars['i']->value]['desigErr'];?>
 ">
-		<input type="hidden" id="year_of_exp_Err_Data_<?php echo $_smarty_tpl->tpl_vars['i']->value;?>
-"  value="<?php echo $_smarty_tpl->tpl_vars['expErr']->value[$_smarty_tpl->tpl_vars['i']->value]['year_of_expErr'];?>
+		<input type="hidden" id="from_year_of_exp_Err_Data_<?php echo $_smarty_tpl->tpl_vars['i']->value;?>
+"  value="<?php echo $_smarty_tpl->tpl_vars['expErr']->value[$_smarty_tpl->tpl_vars['i']->value]['from_year_of_expErr'];?>
 ">
-		<input type="hidden" id="month_of_exp_Err_Data_<?php echo $_smarty_tpl->tpl_vars['i']->value;?>
-"  value="<?php echo $_smarty_tpl->tpl_vars['expErr']->value[$_smarty_tpl->tpl_vars['i']->value]['month_of_expErr'];?>
+		<input type="hidden" id="from_month_of_exp_Err_Data_<?php echo $_smarty_tpl->tpl_vars['i']->value;?>
+"  value="<?php echo $_smarty_tpl->tpl_vars['expErr']->value[$_smarty_tpl->tpl_vars['i']->value]['from_month_of_expErr'];?>
+">
+		<input type="hidden" id="to_year_of_exp_Err_Data_<?php echo $_smarty_tpl->tpl_vars['i']->value;?>
+"  value="<?php echo $_smarty_tpl->tpl_vars['expErr']->value[$_smarty_tpl->tpl_vars['i']->value]['to_year_of_expErr'];?>
+">
+		<input type="hidden" id="to_month_of_exp_Err_Data_<?php echo $_smarty_tpl->tpl_vars['i']->value;?>
+"  value="<?php echo $_smarty_tpl->tpl_vars['expErr']->value[$_smarty_tpl->tpl_vars['i']->value]['to_month_of_expErr'];?>
 ">
 		<input type="hidden" id="workloc_Err_Data_<?php echo $_smarty_tpl->tpl_vars['i']->value;?>
 "  value="<?php echo $_smarty_tpl->tpl_vars['expErr']->value[$_smarty_tpl->tpl_vars['i']->value]['worklocErr'];?>
@@ -1234,11 +1261,17 @@ $(document).ready(function(){
 			if($('#areaData_'+i).length > 0){ 
 				$('#area_'+i).attr('value', $('#areaData_'+i).val());
 			}
-			if($('#year_of_expData_'+i).length > 0){ 
-				$('#year_of_exp_'+i).attr('value', $('#year_of_expData_'+i).val());
+			if($('#from_year_of_expData_'+i).length > 0){ 
+				$('#from_year_of_exp_'+i).attr('value', $('#from_year_of_expData_'+i).val());
 			}
-			if($('#month_of_expData_'+i).length > 0){ 
-				$('#month_of_exp_'+i).val( $('#month_of_expData_'+i).val());
+			if($('#from_month_of_expData_'+i).length > 0){ 
+				$('#from_month_of_exp_'+i).val( $('#from_month_of_expData_'+i).val());
+			}
+			if($('#to_year_of_expData_'+i).length > 0){ 
+				$('#to_year_of_exp_'+i).attr('value', $('#to_year_of_expData_'+i).val());
+			}
+			if($('#to_month_of_expData_'+i).length > 0){ 
+				$('#to_month_of_exp_'+i).val( $('#to_month_of_expData_'+i).val());
 			}
 			if($('#companyData_'+i).length > 0){ 
 				$('#company_'+i).val( $('#companyData_'+i).val());
@@ -1263,11 +1296,17 @@ $(document).ready(function(){
 			if($('#desig_Err_Data_'+i).length > 0){ 
 				$('#desigErr_'+i).html($('#desig_Err_Data_'+i).val());
 			}
-			if($('#year_of_exp_Err_Data_'+i).length > 0){ 
-				$('#year_of_expErr_'+i).html($('#year_of_exp_Err_Data_'+i).val());
+			if($('#from_year_of_exp_Err_Data_'+i).length > 0){ 
+				$('#from_year_of_expErr_'+i).html($('#from_year_of_exp_Err_Data_'+i).val());
 			}
-			if($('#month_of_exp_Err_Data_'+i).length > 0){ 
-				$('#month_of_expErr_'+i).html($('#month_of_exp_Err_Data_'+i).val());
+			if($('#from_month_of_exp_Err_Data_'+i).length > 0){ 
+				$('#from_month_of_expErr_'+i).html($('#from_month_of_exp_Err_Data_'+i).val());
+			}
+			if($('#to_year_of_exp_Err_Data_'+i).length > 0){ 
+				$('#to_year_of_expErr_'+i).html($('#to_year_of_exp_Err_Data_'+i).val());
+			}
+			if($('#to_month_of_exp_Err_Data_'+i).length > 0){ 
+				$('#to_month_of_expErr_'+i).html($('#to_month_of_exp_Err_Data_'+i).val());
 			}
 			if($('#workloc_Err_Data_'+i).length > 0){ 
 				$('#worklocErr_'+i).html($('#workloc_Err_Data_'+i).val());
