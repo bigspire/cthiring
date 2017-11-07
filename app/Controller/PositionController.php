c44f768f7032fbcc305ff65a88d4d5f88c88b801
ravichandran1383 <contact@bigspire.com>
2017-11-07 16:11:42 +0530

    changes

--- app/Controller/PositionController.php
+++ app/Controller/PositionController.php
@@ -98,16 +98,15 @@ class PositionController extends AppController {
 		}
 		
 		// for director and BH		
-		if($this->Session->read('USER.Login.roles_id') == '33' || $this->Session->read('USER.Login.roles_id') == '38'
-		|| $this->Session->read('USER.Login.roles_id') == '39'){
+		if($this->Session->read('USER.Login.roles_id') == '33'){
 			$show = 'all';
-			// $team_cond = false;
+			$team_cond = false;
 		}else{
 			$show = '1';
-			// $team_cond = true;
+			$team_cond = true;
 		}
 		
-		$team_cond = true;
+		
 		
 		// get the team members
 		$result = $this->Position->get_team($this->Session->read('USER.Login.id'),$show);
@@ -138,12 +137,12 @@ class PositionController extends AppController {
 			$roleCond = array('PositionStatus.member_approve' => 'A');
 		}
 		
-		/*
+		
 		
 		// check role based access
-		if($this->Session->read('USER.Login.roles_id') == '34'){ // account holder
+		if($this->Session->read('USER.Login.roles_id') == '34'   && !$team_cond){ // account holder
 			$empCond = array('AH.users_id' => $this->Session->read('USER.Login.id'));
-		}else if($this->Session->read('USER.Login.roles_id') == '30'){ // recruiter
+		}else if($this->Session->read('USER.Login.roles_id') == '30'   && !$team_cond){ // recruiter
 			$empCond = array('OR' => array(
 					'ReqResume.created_by' =>  $this->Session->read('USER.Login.id'),
 					'ReqTeam.users_id' => $this->Session->read('USER.Login.id')
@@ -156,7 +155,7 @@ class PositionController extends AppController {
 			$team_cond = '';
 		}
 		
-		*/
+		
 	
 		// set keyword condition
 		if($this->params->query['keyword'] != ''){
